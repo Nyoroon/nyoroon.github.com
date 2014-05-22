68df6509eb30756f4455809468c7b3b5de3c07ec
Smoked Cheese <root@ubuntology.ru>
2014-05-22 23:12:50 +0200

    main.js

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 gitweb = "https://api.github.com/repos/Nyoroon/nyoroon.github.com/contents/"
+var pages;
 
 $.getJSON(gitweb + "pages", function(data) {
     pages = [];
     for(var i in data) {
         file = data[i];
         filename = file["name"]
-        console.log(filename.substr(0, filename.length - 3))
+        pages.push(filename.substr(0, filename.length - 3))
         //$.getJSON(gitweb + "pages/" + file, function(page_content) {
         //    console.log(page_content);
         //});
     }
 });
+
+var pages_html = $("<div id='pages'>");
+    
+pages_html.append($("<h1>Pages</h1>"));
+for(var page in pages) {
+    pages_html.append($("<li>" + page + "</li>"))
+}
+    
