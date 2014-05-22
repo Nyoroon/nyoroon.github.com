5ff3f90a1bdd63e1687cbaac146ce5a7b635db4e
Smoked Cheese <root@ubuntology.ru>
2014-05-22 23:00:06 +0200

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 gitweb = "https://api.github.com/repos/Nyoroon/nyoroon.github.com/contents/"
 
-$.getJSON(gitweb + "/pages", function(data) {
+$.getJSON(gitweb + "pages", function(data) {
     pages = [];
     for(var file in data) {
-        $.getJSON(gitweb + "/pages/" + file, function(page_content) {
+        $.getJSON(gitweb + "pages/" + file, function(page_content) {
             console.log(page_content);
         });
     }
