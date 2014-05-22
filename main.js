a21c9bf31a74f467351da1fc6461e26559131aa7
Smoked Cheese <root@ubuntology.ru>
2014-05-22 23:05:47 +0200

    main.js

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ $.getJSON(gitweb + "pages", function(data) {
     pages = [];
     for(var i in data) {
         file = data[i];
-        console.log(file.substr(0, file.length - 3))
+        filename = file["name"]
+        console.log(filename.substr(0, filename.length - 3))
         //$.getJSON(gitweb + "pages/" + file, function(page_content) {
         //    console.log(page_content);
         //});
